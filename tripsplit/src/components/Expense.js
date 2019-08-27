e5ccaac364972fe0a1d94184d08ee13b0d5c9b0d
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-08-27 11:55:49 -0400

    Add boilerplate for expense component and expense list

---
+++ tripsplit/src/components/Expense.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Expense() {
+    return <div className="expense">An expense</div>;
+}
+
+export default Expense;
---
+++ tripsplit/src/components/ExpensesList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Expense from './Expense';
+
+function ExpensesList() {
+    return <div className="expenses-list">List of expenses</div>;
+}
+
+export default ExpensesList;
