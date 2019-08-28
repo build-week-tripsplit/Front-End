5bc128e49f82fdc5660926eebc3680275de8211f
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-28 15:52:13 -0400

    individualTripContainer now displays trip info and a list of expenses

--- tripsplit/src/components/IndividualTripContainer.js
+++ tripsplit/src/components/IndividualTripContainer.js
@@ -3,22 +3,27 @@ import Header from "./header/Header";
 import NavBar from "./header/NavBar"
 import axios from "axios";
 import Trip from './Trip';
+import Expense from "./Expense";
 
 const IndividualTrip = (props) => {
     const id = props.match.params.id;
     const [trip, setTrip] = useState({});
+    const [tripExpenses, setTripExpenses] = useState([]);
     useEffect(() => {
         axios.get(`https://tripsplit-backend.herokuapp.com/api/trips/${id}`)
             .then(response => setTrip(response.data))
-            .catch(error => console.log('Error: IndividualTripContainer.js: ', error))
+            .catch(error => console.log('Error: IndividualTripContainer.js: TripGet: ', error));
+
+        axios.get(`https://tripsplit-backend.herokuapp.com/api/expenses`)
+            .then(response => setTripExpenses(response.data.filter(expense => expense.trip_id === Number(id))))
+            .catch(error => console.log('Error: IndividualTripContainer.js: ExpenseGet: ', error));
     }, [])
     return (
         <>
             <Header />
             <NavBar />
             <Trip key={trip.id} trip={trip} />
-
-            {/* Expense List Component Here */}
+            {tripExpenses.map(expense => <Expense expense={expense} />)}
         </>
     )
 }
