e16931febe62e983fd8fec6a77a64018ee202f09
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-28 13:37:22 -0400

    individual trip container now gets trip info from backend and displays it for a specific trip

--- tripsplit/src/components/IndividualTripContainer.js
+++ tripsplit/src/components/IndividualTripContainer.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./header/Header";
 import NavBar from "./header/NavBar"
+import axios from "axios";
+import Trip from './Trip';
 
-const IndividualTrip = () => {
+const IndividualTrip = (props) => {
+    const id = props.match.params.id;
+    const [trip, setTrip] = useState({});
+    useEffect(() => {
+        axios.get(`https://tripsplit-backend.herokuapp.com/api/trips/${id}`)
+            .then(response => setTrip(response.data))
+            .catch(error => console.log('Error: IndividualTripContainer.js: ', error))
+    }, [])
     return (
         <>
             <Header />
             <NavBar />
-            {/* Individual Trip Information Here */}
+            <Trip key={trip.id} trip={trip} />
 
             {/* Expense List Component Here */}
         </>
