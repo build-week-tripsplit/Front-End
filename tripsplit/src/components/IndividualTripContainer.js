916272766cf656775df86e8de3aac73ab5371abc
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-28 17:04:03 -0400

    summary appears on individualtripcontainer.js

--- tripsplit/src/components/IndividualTripContainer.js
+++ tripsplit/src/components/IndividualTripContainer.js
@@ -10,6 +10,7 @@ const IndividualTrip = (props) => {
     const id = props.match.params.id;
     const [trip, setTrip] = useState({});
     const [tripExpenses, setTripExpenses] = useState([]);
+    const [totalCost, setTotalCost] = useState(0);
     useEffect(() => {
         axios.get(`https://tripsplit-backend.herokuapp.com/api/trips/${id}`)
             .then(response => setTrip(response.data))
@@ -17,7 +18,15 @@ const IndividualTrip = (props) => {
 
         //Change endpoint to grab user specific expsenses. Or user & trip specific expenses
         axios.get(`https://tripsplit-backend.herokuapp.com/api/expenses`)
-            .then(response => setTripExpenses(response.data.filter(expense => expense.trip_id === Number(id))))
+            .then(response => {
+                let filteredExpenses = (response.data.filter(expense => expense.trip_id === Number(id)));
+                setTripExpenses(filteredExpenses);
+                return filteredExpenses;
+            })
+            .then(response => {
+                let expenseAmounts = response.map(item => item.amount);
+                setTotalCost(expenseAmounts.reduce(((acc, curr) => acc + curr), 0))
+            })
             .catch(error => console.log('Error: IndividualTripContainer.js: ExpenseGet: ', error));
     }, [])
     return (
@@ -25,6 +34,7 @@ const IndividualTrip = (props) => {
             <Header />
             <NavBar />
             <Trip key={trip.id} trip={trip} />
+            <h2>Your Summary: ${totalCost} spent.</h2>
             <Link to="/expenseform"><button>Create Expense</button></Link>
             {tripExpenses.map(expense => <Expense expense={expense} />)}
         </>
