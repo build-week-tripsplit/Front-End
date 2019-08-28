6ca49a2bbcadd91f1a160919877b5b00c8789d20
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-28 17:01:21 -0400

    Styled the landing page.

--- tripsplit/src/components/LandingPage.js
+++ tripsplit/src/components/LandingPage.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+
 
 const LandingPage = () => {
+
+    const landingStyle = {
+        backgroundImage: 'url(./../landing_img.jpg)',
+        backgroundRepeat: 'no-repeat',
+        position: 'fixed',
+        backgroundPosition: 'center',
+        marginTop: '30px',
+    };
+
+
+
     return (
-        <>
-            <Link to="/login"><button>Login</button></Link>
-            <Link to="/register"><button>Register</button></Link>
-        </>
+
+        <div className="landing-page" style={landingStyle}>
+
+            <div className="landing-div">
+            <h1>TripSplit</h1>
+            {/* <img className="landing_img" src='./../landing_img.jpg' alt="Two people using the TripSplit app."></img> */}
+            <h4>Keep track of group expenses</h4>
+            <p>No more IOU's</p>
+            <Link to="/register"><button className="button-style-main">Sign Up</button></Link>
+
+            <div className="sign-in-div">
+            <p>Already have an account?</p>
+            <Link className="sign-in-link" to="/login">Sign In</Link>
+            </div>
+
+            </div>
+
+        </div>
     )
 }
 
