4d1c87ae65c222d23820316ef8d2e351bdb29926
doanmade <43007082+doanmade@users.noreply.github.com>
2019-08-29 11:12:25 -0700

    need to pull

--- tripsplit/src/components/forms/TripForm.js
+++ tripsplit/src/components/forms/TripForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const TripForm = () => {
   const [state, setState] = useState({});
