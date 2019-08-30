cab9bc893e6151df2f45ea074a2205d63502d8c2
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-29 22:43:14 -0400

    added logo to expenses and got rid of welcome back on dashboard

--- tripsplit/src/components/Expense.js
+++ tripsplit/src/components/Expense.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { deleteExpense } from '../actions/deleteExpense';
+import { Icon } from "semantic-ui-react"
 
 const Expense = ({ expense, deleteExpense }) => {
     //Pass in props data from ExpenseList.
@@ -9,12 +10,12 @@ const Expense = ({ expense, deleteExpense }) => {
         //JSX element displaying a card for every indvidual expense.
         <div className="expense">
             <div className="expense-name-date">
-            <h3>{expense.title}</h3>
-            <h6>{expense.date}</h6>
+                <h3>{expense.title}</h3>
+                <h6>{expense.date}</h6>
             </div>
 
             <p>${expense.amount}</p>
-            <button className="delete-expense" onClick={() => deleteExpense(expense)}>X</button>
+            <button className="delete-expense" onClick={() => deleteExpense(expense)}><Icon name="delete" /></button>
         </div>
     );
 };
