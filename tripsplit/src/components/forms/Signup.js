14e20bb0463f7223c6c2e358d7f024c79da59aa0
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-29 21:35:31 -0400

    added header to login and signup and modified header to have different logo

--- tripsplit/src/components/forms/Signup.js
+++ tripsplit/src/components/forms/Signup.js
@@ -2,47 +2,50 @@ import axios from 'axios';
 import { Field, Form, withFormik } from 'formik';
 import React from 'react';
 import * as Yup from 'yup';
-
+import Header from "../header/Header";
 const SignUpForm = ({ errors, touched, status }) => {
     return (
-        <div className="sign-up-form">
-            <h3>Let's Get Started</h3>
+        <>
+            <Header />
+            <div className="sign-up-form">
+                <h3>Let's Get Started</h3>
 
-            <Form>
-                <div className="sign-up-fields">
-                    <div className="names-div">
-                        <div className="form-input name field-item-Fname">
-                            <Field type="text" name="firstName" placeholder="First Name" />
-                            {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
+                <Form>
+                    <div className="sign-up-fields">
+                        <div className="names-div">
+                            <div className="form-input name field-item-Fname">
+                                <Field type="text" name="firstName" placeholder="First Name" />
+                                {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
+                            </div>
+
+                            <div className="form-input name field-item-Lname">
+                                <Field type="text" name="lastName" placeholder="Last Name" />
+                                {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
+                            </div>
                         </div>
 
-                        <div className="form-input name field-item-Lname">
-                            <Field type="text" name="lastName" placeholder="Last Name" />
-                            {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
+                        <div className="form-input field-item">
+                            <Field type="email" name="email" placeholder="Email Address" />
+                            {touched.email && errors.email && <p>{errors.email}</p>}
+                        </div>
+
+                        <div className="form-input field-item">
+                            <Field type="text" name="username" placeholder="Username" />
+                            {touched.username && errors.username && <p>{errors.username}</p>}
+                        </div>
+
+                        <div className="form-input field-item-last">
+                            <Field type="text" name="password" placeholder="Password" />
+                            {touched.password && errors.password && <p>{errors.password}</p>}
                         </div>
                     </div>
 
-                    <div className="form-input field-item">
-                        <Field type="email" name="email" placeholder="Email Address" />
-                        {touched.email && errors.email && <p>{errors.email}</p>}
-                    </div>
-
-                    <div className="form-input field-item">
-                        <Field type="text" name="username" placeholder="Username" />
-                        {touched.username && errors.username && <p>{errors.username}</p>}
-                    </div>
-
-                    <div className="form-input field-item-last">
-                        <Field type="text" name="password" placeholder="Password" />
-                        {touched.password && errors.password && <p>{errors.password}</p>}
-                    </div>
-                </div>
-
-                <button className="button-style-main" type="submit">
-                    Join
+                    <button className="button-style-main" type="submit">
+                        Join
                 </button>
-            </Form>
-        </div>
+                </Form>
+            </div>
+        </>
     );
 };
 
